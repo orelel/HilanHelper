b0b981a0c445b399e8def50e8692d7d4795d0fc1
Orel Eliyahu <oeliyahu@interwise.com>
2017-12-27 11:25:06 +0200

    add MutationObserver to watch dom changes

--- js/core/dom-manipulation.ts
+++ js/core/dom-manipulation.ts
@@ -1,13 +1,40 @@
 declare var $;
+const CURRENT_MONTH_SELECTOR = '#ctl00_mp_currentMonth';
+const CALENDAR_CONTAINER_SELECTOR = '#ctl00_mp_calendarUpdator'
 
-enum CalendarData{
+enum CalendarData {
     HOURS = 1,
     TEXT = 2
 }
 
-class DomManipulation{
+class DomManipulation {
 
-    public static getIframeContent = () => {
+    public static watchElement(pageContent, selector, callbackFn) {
+        
+        // Options for the observer (which mutations to observe)
+        var config = { attributes: true, childList: true, subtree:true };
+
+        // Callback function to execute when mutations are observed
+        var callback = function (mutationsList) {
+            for (var mutation of mutationsList) {
+                if (mutation.type == 'childList') {
+                    callbackFn();
+                }               
+            }
+        };
+
+        // Create an observer instance linked to the callback function
+        var observer = new MutationObserver(callback);
+
+        // Start observing the target node for configured mutations
+        observer.observe($(pageContent.querySelector(selector))[0], config);
+    }
+
+    public static getCurrentMonthAndYear(pageContent){
+        return pageContent.querySelector(CURRENT_MONTH_SELECTOR).value;
+    }
+
+    public static getIframeContent() {
         var dfd = $.Deferred();
         $(document).ready(() => {
             const iframe: any = document.querySelector('#mainIFrame');
@@ -33,7 +60,7 @@ class DomManipulation{
                         <span>${options.currentMonth}</span>
                     </li>
                     <li class='should-work-hours'>
-                        <span>שעות מצופות</span>
+                        <span>שעות תקן</span>
                         <span>${options.shouldWorkHours}</span>
                     </li>
                     <li class='work-hours'>
@@ -53,36 +80,18 @@ class DomManipulation{
         );
     }
 
-    public static addButtonListener = (pageContent, selector, callbackFn) => {
+    public static addButtonListener(pageContent, selector, callbackFn) {
         $(pageContent.querySelector(selector)).ready(() => {
             $(pageContent.querySelector(selector)).on('click', callbackFn);
         });
 
     }
 
-    public static getCalendarMonthData(pageContent,index:CalendarData){
+    public static getCalendarMonthData(pageContent, index: CalendarData) {
         const arr = [];
         [].forEach.call(pageContent.querySelectorAll(`#calendar_container tr:nth-child(n+3) tr:nth-child(${index}) td:nth-child(1)`), (elem) => {
             arr.push($(elem).text())
         });
-        return arr;       
+        return arr;
     }
-
-
-        // private isHtmlReady = (selector) => {
-    //     var dfd = $.Deferred();
-    //     console.log($(selector).text())
-    //     if ($(selector).text()) {            
-    //         dfd.resolve($(selector));
-    //     }
-
-    //     console.log(dfd.state())
-    //     if (dfd.state() === 'pending') {            
-    //         setTimeout(() => this.isHtmlReady,10);
-    //     }
-
-    //     return dfd.promise();
-    // }
-
-   
 }
